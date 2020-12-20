1a18447faf23292dcf745dd0535328742fecac0d
MatthewP <mpetrus001@gmail.com>
2020-12-19 20:45:01 -0600

    add apollo with HelloWorld resolver middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(); // uses config from src/mikro-orm.config.ts
@@ -8,9 +11,14 @@ const main = async () => {
 
   const app = express();
 
-  app.get("/", (_, res) => {
-    res.end("hello, world!");
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
   });
+
+  apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
     console.log("server listening on port 4000");
