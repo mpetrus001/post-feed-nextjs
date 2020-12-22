13d2e80caa9b4a87a4c1d38ec0aa29d9035c9094
MatthewP <mpetrus001@gmail.com>
2020-12-21 22:42:19 -0600

    add  urql chage refresh on login/register

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,77 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
-import { createClient, Provider } from "urql";
+import { cacheExchange, QueryInput, Cache } from "@urql/exchange-graphcache";
+import { AppProps } from "next/dist/next-server/lib/router/router";
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import {
+  LoginUserMutation,
+  MeDocument,
+  MeQuery,
+  RegisterUserMutation,
+} from "../generated/graphql";
 import theme from "../theme";
+
+// added for improved typing
+function typedUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: { credentials: "include" },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          registerUser: (_result, args, cache, info) => {
+            typedUpdateQuery<RegisterUserMutation, MeQuery>(
+              cache,
+              {
+                query: MeDocument,
+              },
+              _result,
+              (result, query) => {
+                if (result.registerUser.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.registerUser.user,
+                  };
+                }
+              }
+            );
+          },
+          loginUser: (_result, args, cache, info) => {
+            typedUpdateQuery<LoginUserMutation, MeQuery>(
+              cache,
+              {
+                query: MeDocument,
+              },
+              _result,
+              (result, query) => {
+                if (result.loginUser.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.loginUser.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <ColorModeProvider
