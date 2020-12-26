177639cf027993b72cca4861b4ff55cc0dc1bdd3
MatthewP <mpetrus001@gmail.com>
2020-12-25 21:01:11 -0600

    implemented apollo-graphql errors to simplify res

--- backend/src/resolvers/user.ts
+++ backend/src/resolvers/user.ts
@@ -1,5 +1,4 @@
 import { User } from "../entities/User";
-// import { getRepository } from "typeorm";
 import {
   Resolver,
   Query,
@@ -8,6 +7,8 @@ import {
   Arg,
   ObjectType,
   Field,
+  UseMiddleware,
+  InputType,
 } from "type-graphql";
 import { MyContext } from "src/types";
 import argon2 from "argon2";
@@ -15,15 +16,19 @@ import { nanoid } from "nanoid";
 import { COOKIE_NAME } from "../constants";
 import { sendEmail } from "../utils/sendEmail";
 import { FieldError } from "./types";
+import { requireAuth } from "../middleware/requireAuth";
+import { AuthenticationError, UserInputError } from "apollo-server-express";
 
-@ObjectType()
-class UserResponse {
-  // Field defines what types show up in GraphQL
-  @Field(() => [FieldError], { nullable: true })
-  errors?: FieldError[];
+@InputType()
+class UserInput {
+  @Field(() => String)
+  username: string;
 
-  @Field(() => User, { nullable: true })
-  user?: User | null;
+  @Field(() => String)
+  email: string;
+
+  @Field(() => String)
+  password: string;
 }
 
 // set up the GraphQL resolvers
@@ -33,35 +38,39 @@ export class UserResolver {
   @Query(() => User, { nullable: true })
   async me(
     @Ctx() { req, orm: { UserRepository } }: MyContext
-  ): Promise<User | undefined> {
+  ): Promise<User | null> {
     const id = req.session.userId;
-    if (!id) return undefined;
-    return UserRepository.findOne({ id });
+    if (!id) return null;
+    const user = await UserRepository.findOne({ id });
+    if (!user) return null;
+    return user;
   }
 
   // returns all users
-  @Query(() => [User])
+  @Query(() => [User]) // sets the type-graphql return type
+  @UseMiddleware(requireAuth) // will throw if user session doesn't exist
   users(@Ctx() { orm: { UserRepository } }: MyContext): Promise<User[]> {
     return UserRepository.find({});
   }
 
   // returns a single user by id
   @Query(() => User, { nullable: true })
-  user(
+  @UseMiddleware(requireAuth)
+  async user(
     @Arg("id") id: number,
     @Ctx() { orm: { UserRepository } }: MyContext
-  ): Promise<User | undefined> {
-    return UserRepository.findOne({ id });
+  ): Promise<User | null> {
+    const user = await UserRepository.findOne({ id });
+    // typeorm would return undefined, but i prefer null
+    return user ? user : null;
   }
 
   // adds a new user
-  @Mutation(() => UserResponse)
+  @Mutation(() => User)
   async registerUser(
-    @Arg("username") username: string,
-    @Arg("email") email: string,
-    @Arg("password") password: string,
+    @Arg("userInput") { username, email, password }: UserInput,
     @Ctx() { req, orm: { UserRepository } }: MyContext
-  ): Promise<UserResponse> {
+  ): Promise<User> {
     // validate each of the inputs
     let errors: FieldError[] = [];
     if (username.length < 2)
@@ -84,7 +93,23 @@ export class UserResolver {
         field: "email",
         message: "email must be at least 6 chars",
       });
-    if (errors.length > 0) return { errors };
+    if (!email.includes("@"))
+      errors.push({
+        field: "email",
+        message: "email must contain an @ symbol",
+      });
+    if (errors.length > 0) {
+      let fields = errors.reduce(
+        (acc, error) => [...acc, error.field],
+        [] as string[]
+      );
+      throw new UserInputError(
+        `${JSON.stringify(fields)} did not pass format validation`,
+        {
+          fieldErrors: errors,
+        }
+      );
+    }
 
     // if all validation passes then proceed with register
     try {
@@ -97,66 +122,59 @@ export class UserResolver {
       await UserRepository.save(newUser);
       // create a session for the new user and sends a cookie
       req.session.userId = newUser.id;
-      return { user: newUser };
+      return newUser;
     } catch (error) {
       if (
         error.detail.includes("username") &&
         error.detail.includes("already exists")
       ) {
-        return {
-          errors: [
+        throw new UserInputError("username already exists", {
+          fieldErrors: [
             {
               field: "username",
               message: "username already exists",
             },
           ],
-        };
-      } else if (
+        });
+      }
+      if (
         error.detail.includes("email") &&
         error.detail.includes("already exists")
       ) {
-        return {
-          errors: [
+        throw new UserInputError("email already exists", {
+          fieldErrors: [
             {
               field: "email",
               message: "email already exists",
             },
           ],
-        };
-      } else {
-        return {
-          errors: [
-            {
-              field: "server",
-              message: "unexpected error",
-            },
-          ],
-        };
+        });
       }
+      throw error;
     }
   }
 
   // logs in an existing user
-  @Mutation(() => UserResponse)
+  @Mutation(() => User)
   async loginUser(
     @Arg("username") usernameOrEmail: string,
     @Arg("password") password: string,
     @Ctx() { req, orm: { UserRepository } }: MyContext
-  ): Promise<UserResponse> {
+  ): Promise<User> {
     const matchedUser = await UserRepository.findOne(
       usernameOrEmail.includes("@")
         ? { email: usernameOrEmail }
         : { username: usernameOrEmail }
     );
     if (!matchedUser)
-      return {
-        errors: [
+      throw new UserInputError("username could not be found", {
+        fieldErrors: [
           {
             field: "username",
-            message: "login attempt failed",
+            message: "username could not be found",
           },
         ],
-      };
+      });
     const isMatchingPassword = await argon2.verify(
       matchedUser.password,
       password
@@ -164,21 +182,22 @@ export class UserResolver {
     if (isMatchingPassword) {
       // create a session for the logged in user and sends a cookie
       req.session.userId = matchedUser.id;
-      return { user: matchedUser };
+      return matchedUser;
+    } else {
+      throw new UserInputError("password did not match", {
+        fieldErrors: [
+          {
+            field: "password",
+            message: "password did not match",
+          },
+        ],
+      });
     }
-    return {
-      errors: [
-        {
-          field: "username",
-          message: "login attempt failed",
-        },
-      ],
-    };
   }
 
   // logs user out of the session
   @Mutation(() => Boolean)
-  async logoutUser(@Ctx() { req, res }: MyContext): Promise<Boolean> {
+  async logoutUser(@Ctx() { req, res }: MyContext): Promise<boolean> {
     return new Promise((resolve) =>
       req.session.destroy((err) => {
         if (err) {
@@ -192,21 +211,27 @@ export class UserResolver {
   }
 
   // sends password reset and logs user out of the session
-  @Mutation(() => UserResponse)
+  @Mutation(() => Boolean)
   async resetPassword(
     @Ctx() { redis, req, res, orm: { UserRepository } }: MyContext,
-    @Arg("email") email: string
-  ): Promise<UserResponse> {
-    const matchedUser = await UserRepository.findOne({ email });
+    @Arg("username") usernameOrEmail: string
+  ): Promise<boolean> {
+    const matchedUser = await UserRepository.findOne(
+      usernameOrEmail.includes("@")
+        ? { email: usernameOrEmail }
+        : { username: usernameOrEmail }
+    );
     if (!matchedUser)
-      return {
-        errors: [
+      throw new UserInputError("username could not be found", {
+        fieldErrors: [
           {
-            field: "email",
-            message: "email could not be found",
+            field: "username",
+            message: "username could not be found",
           },
         ],
-      };
+      });
+
+    // set a token for matching the user in the password reset email
     const token = nanoid();
     await redis.set(
       `reset-password-${token}`,
@@ -215,80 +240,66 @@ export class UserResolver {
       1000 * 60 * 60 * 8 // 8 hours
     );
 
+    // send the password reset email with link
     sendEmail({
-      to: email,
+      to: matchedUser.email,
       subject: "Password Reset Link - Reddit-Clone",
       html: `<a href="http://localhost:3000/reset-password/${token}">Reset password</a>`,
     });
-    return new Promise((resolve) =>
-      req.session.destroy((err) => {
-        if (err) {
-          console.log(err);
-          resolve({ user: null });
-        }
-        res.clearCookie(COOKIE_NAME);
-        resolve({ user: null });
-      })
-    );
+
+    // log the user out of the client side and remove their session
+    req.session.destroy((err) => {
+      if (err) console.log(err);
+    });
+    res.clearCookie(COOKIE_NAME);
+
+    return true;
   }
 
   // change a users password
-  @Mutation(() => UserResponse)
+  @Mutation(() => User)
   async changePassword(
     @Arg("password") password: string,
     @Arg("token") token: string,
     @Ctx() { redis, orm: { UserRepository } }: MyContext
-  ): Promise<UserResponse> {
+  ): Promise<User> {
     // check if the reset token is valid
-    const matchingId = await redis.get(`reset-password-${token}`);
-    if (!matchingId)
-      return {
-        errors: [
-          {
-            field: "password",
-            message: "token is invalid",
-          },
-        ],
-      };
+    const storedId = await redis.get(`reset-password-${token}`);
+    if (!storedId)
+      throw new AuthenticationError(
+        "a valid token must be included in request"
+      );
     if (password.length < 6)
-      return {
-        errors: [
-          {
-            field: "password",
-            message: "password must be at least 6 chars",
-          },
-        ],
-      };
+      // stringifying as a string to match earlier format validation response
+      throw new UserInputError(
+        `${JSON.stringify(["password"])} did not pass format validation`,
+        {
+          fieldErrors: [
+            {
+              field: "password",
+              message: "password must be at least 6 chars",
+            },
+          ],
+        }
+      );
 
     // if all validation passes then proceed with reset
-    // redis stores strings but postgres ids are int
-    const id = parseInt(matchingId);
+    // redis stores strings but postgres ids are type int
+    const id = parseInt(storedId);
     try {
       const matchedUser = await UserRepository.findOne({ id });
       if (!matchedUser)
-        return {
-          errors: [
-            {
-              field: "password",
-              message: "token is invalid",
-            },
-          ],
-        };
+        throw new AuthenticationError(
+          "a valid token must be included in request"
+        );
       const hash = await argon2.hash(password);
       matchedUser.password = hash;
       await UserRepository.save(matchedUser);
       // remove the reset-password token from the session
       redis.del(`reset-password-${token}`);
-      return { user: matchedUser };
+      return matchedUser;
     } catch (error) {
-      return {
-        errors: [
-          {
-            field: "server",
-            message: "unexpected error",
-          },
-        ],
-      };
+      throw error;
     }
   }
 
@@ -306,13 +317,13 @@ export class UserResolver {
   // }
 
   // delete a single user by id
-  @Mutation(() => Boolean)
-  async deleteUser(
-    @Arg("id") id: number,
-    @Ctx() { orm: { UserRepository } }: MyContext
-  ): Promise<boolean> {
-    const { affected } = await UserRepository.delete({ id });
-    if (affected && affected > 0) return true;
-    return false;
-  }
+  // @Mutation(() => Boolean)
+  // async deleteUser(
+  //   @Arg("id") id: number,
+  //   @Ctx() { orm: { UserRepository } }: MyContext
+  // ): Promise<boolean> {
+  //   const { affected } = await UserRepository.delete({ id });
+  //   if (affected && affected > 0) return true;
+  //   return false;
+  // }
 }
